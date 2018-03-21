7e31c9721aa3ec3ab9d55b5f6e412dee7b6fc7f8
邓江洲 <dengjingzhou@zto.cn>
2018-03-21 10:23:32 +0800

    ray 2.0 start project做好了

--- README.md
+++ README.md
@@ -6,16 +6,49 @@ ReactiveLearn, via Ray Wenderlich
 
 <hr>
 
+
+<q>
+ It can take a bit of practice to get used to the concepts, but like any language or program, once you get the hang of it it’s really quite simple. At the very core of ReactiveCocoa are signals, which are nothing more than streams of events. What could be simpler than that?
+
+
+</q>
+
+<hr>
+
+
+<hr>
+
+
+
 <ul>
-<li>
-<a href="https://github.com/ReactiveCocoa/ReactiveObjC">ReactiveCocoa/ReactiveObjC</a>
-</li>
+<h3>Github </h3>
+<br>
 
 <li>
+
+
+<a href="https://github.com/ReactiveCocoa/ReactiveObjC">ReactiveCocoa/ReactiveObjC</a>
+</li>
+<h3>MS Developer Net </h3>
+<br>
+
+<li>
+
 <a href="https://msdn.microsoft.com/library/hh242985.aspx">MSDN: Reactive Extensions</a>
 
 
 </li>
+
+<h3>Ray Wenderlich </h3>
+<br>
+
+<li>
+
+<a href="https://msdn.microsoft.com/library/hh242985.aspx">ReactiveCocoa Tutorial – The Definitive Introduction: Part 1/2</a>
+
+
+</li>
+
 
 </ul>
 <hr>
--- Reactive/SceneOne/RWViewController.m
+++ Reactive/SceneOne/RWViewController.m
@@ -58,10 +58,19 @@
         self.signInButton.enabled = signupActive.boolValue;
     }];
     
-    [[self.signInButton rac_signalForControlEvents: UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
-        NSLog(@"Button Clicked");
+  
+    [[[[self.signInButton rac_signalForControlEvents: UIControlEventTouchUpInside ] doNext:^(__kindof UIControl * _Nullable x) {
+        self.signInButton.enabled = NO;
+        self.signInFailureText.hidden = YES;
+    }] flattenMap:^__kindof RACSignal * _Nullable(__kindof UIControl * _Nullable value) {
+        return [self signInSignal];
+    }] subscribeNext:^(NSNumber * successValid) {
+        BOOL isSucceed = successValid.boolValue;
+        self.signInFailureText.hidden = isSucceed;
+        if (isSucceed) {
+            [self performSegueWithIdentifier: @"signInSuccess" sender:self];
+        }
     }];
-    
     
 }
 
@@ -91,6 +100,14 @@
     }];
 
 }
+/*
+ The above method creates a signal that signs in with the current username and password. Now for a breakdown of its component parts.
+ The above code uses the createSignal: method on RACSignal for signal creation. The block that describes this signal is a single argument, and is passed to this method. When this signal has a subscriber, the code within this block executes.
+ The block is passed a single subscriber instance that adopts the RACSubscriber protocol, which has methods you invoke in order to emit events; you may also send any number of next events, terminated with either an error or complete event. In this case, it sends a single next event to indicate whether the sign-in was a success, followed by a complete event.
+ The return type for this block is an RACDisposable object, and it allows you to perform any clean-up work that might be required when a subscription is cancelled or trashed. This signal does not have any clean-up requirements, hence nil is returned.
+ As you can see, it’s surprisingly simple to wrap an asynchronous API in a signal!
+ 
+ */
 
 
 
@@ -219,3 +236,62 @@ RACSignal * filterUsernameSignal = [usernameSourceSignal filter:^BOOL(id  _Nulla
  */
 
 
+#pragma mark - step Eight
+
+
+
+
+/*  [[self.signInButton rac_signalForControlEvents: UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+ NSLog(@"Button Clicked");
+ }];
+ 
+ 
+ 
+[[[self.signInButton rac_signalForControlEvents: UIControlEventTouchUpInside] map:^RACSignal *(__kindof UIControl * _Nullable value) {
+    return [self signInSignal];
+}] subscribeNext:^(id  _Nullable name) {
+    NSLog(@"%@", name);
+}];
+
+ 
+ The rac_signalForControlEvents emits a next event (with the source UIButton as its event data) when you tap the button. The map step creates and returns the sign-in signal, which means the following pipeline steps now receive a RACSignal. That is what you’re observing at the subscribeNext: step.
+ The situation above is sometimes called the signal of signals; in other words an outer signal that contains an inner signal. If you really wanted to, you could subscribe to the inner signal within the outer signal’s subscribeNext: block. However it would result in a nested mess! Fortunately, it’s a common problem, and ReactiveCocoa is ready for this scenario.
+ 
+ 
+ Signal of Signals
+ The solution to this problem is straightforward, just change the map step to a flattenMap step as shown below:
+ 
+ 
+ 
+ 
+ [[[self.signInButton rac_signalForControlEvents: UIControlEventTouchUpInside ] flattenMap:^__kindof RACSignal * _Nullable(__kindof UIControl * _Nullable value) {
+ return [self signInSignal];
+ }] subscribeNext:^(id  _Nullable x) {
+ NSLog(@"Sign in result: %@", x );
+ }];
+ 
+ */
+
+
+#pragma mark - step Nine
+
+
+/*
+ 
+ Did you notice there is one small user experience issue with the current application? When the sign-in service validates the supplied credentials, is should disable the Sign In button. This prevents the user from repeating the same sign-in. Furthermore, if a failed sign-in attempt occurred, the error message should be hidden when the user tries to sign-in once again.
+ But how should you add this logic to the current pipeline? Changing the button’s enabled state isn’t a transformation, filter or any of the other concepts you’ve encountered so far. Instead, it’s what is known as a side-effect; or logic you want to execute within a pipeline when a next event occurs, but it does not actually change the nature of the event itself.
+
+ 
+ 
+ 
+ [[[self.signInButton rac_signalForControlEvents: UIControlEventTouchUpInside ] flattenMap:^__kindof RACSignal * _Nullable(__kindof UIControl * _Nullable value) {
+ return [self signInSignal];
+ }] subscribeNext:^(NSNumber * successValid) {
+ BOOL isSucceed = successValid.boolValue;
+ self.signInFailureText.hidden = isSucceed;
+ if (isSucceed) {
+ [self performSegueWithIdentifier: @"signInSuccess" sender:self];
+ }
+ }];
+ 
+ */
